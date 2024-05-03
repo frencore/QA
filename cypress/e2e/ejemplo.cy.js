cd2e69cb6052ffe248083c7e65c1f6074d5d280f
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-05-03 12:46:45 -0400

    Creacion primera prueba de ejemplo de igualdad: ejemplo.cy.js

--- cypress/e2e/ejemplo.cy.js
+++ cypress/e2e/ejemplo.cy.js
@@ -4,4 +4,11 @@ describe('template spec', () => {
   })
 })
 
-describe('Test ejemplo',);
+describe('Test ejemplo', () => {
+  it('Descripcion Prueba-puede ser numero de ticket', () => {
+    let a = 1 + 1;
+    let b = 2;
+    expect(a == b).to.equal(true);
+
+  })
+});
