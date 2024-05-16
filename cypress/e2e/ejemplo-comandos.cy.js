5bb8470312a4b04357aafc0b1c86b6733cfce024
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-05-16 15:50:24 -0400

    Se agrega comando select y .eq() para elementos con el mismo id: ejemplo-comandos.cy.js

--- cypress/e2e/ejemplo-comandos.cy.js
+++ cypress/e2e/ejemplo-comandos.cy.js
@@ -1,10 +1,9 @@
 
 
-describe('Comandos con Cypress', () => {
+describe('Comandos con Cypress checkbox y radio', () => {
 
   beforeEach('Funcionamiento de comandos para checkbox y radio', () => {
     cy.visit('https://example.cypress.io/commands/actions');
-    cy.wait(1000);
   });
 
 
@@ -32,4 +31,36 @@ describe('Comandos con Cypress', () => {
     cy.get('.action-radios [type="radio"]').should('be.checked');
   });
 
+});
+
+describe('Comandos con Cypress Select',()=>{
+  beforeEach('Funcionamiento de comandos para Select', () => {
+    cy.visit('https://example.cypress.io/commands/actions');
+    cy.wait(1000);
+  });
+
+  it('Valida el valor de la primera opcion',()=>{
+    cy.get('.action-select').should('have.value', '--Select a fruit--');
+  });
+
+  it('Selecciona la opcion que hace match',()=>{
+    cy.get('.action-select').select('apples');
+    cy.log('confirma seleccion de apples');
+    cy.get('.action-select').should('have.value', 'fr-apples');
+  });
+
+  it('Seleccion multiple opciones',()=>{
+    cy.get('.action-select-multiple').select(['apples', 'oranges', 'bananas']);
+    cy.log('al obtener multiples valores seleccionados los compara usando chai');
+    cy.get('.action-select-multiple').invoke('val').should('deep.equal', ['fr-apples', 'fr-oranges', 'fr-bananas']);
+  });
+
+ 
+});
+
+describe('Para obtener un elemento DOM en un índice específico',()=>{
+  it('como usar el comando .eq() para multiples elementos con el mismo id',()=>{
+    cy.visit('https://example.cypress.io/commands/traversal');
+    cy.get('.traversal-list>li').eq(1).should('contain', 'siamese');
+  });
 });
