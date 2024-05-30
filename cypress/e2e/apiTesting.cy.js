f667596769b7ba20db82d7a9b4ab97ea9f40e5e5
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-05-30 14:36:28 -0400

    se actualizan pruebas de apiTesting se agrega dependencias se importan cypress-plugin-api : apiTesting.cy.js - e2e.js - package.json

--- cypress/e2e/apiTesting.cy.js
+++ cypress/e2e/apiTesting.cy.js
@@ -4,12 +4,12 @@
 describe('pruebas de apis', () => {
 
     it('Valida status 200', () => {
-        cy.request('GET', `${Cypress.env('urlApi')}/ditto`
+        cy.api('GET', `${Cypress.env('urlApi')}/ditto`
         ).then((response) => {
             expect(response.status).to.equal(200);
             expect(response).property('status').to.equal(200);
             //imprime la respuesta JSON
-            cy.log(JSON.stringify(response, null, "\t"));
+           //cy.log(JSON.stringify(response, null, "\t"));
             //imprime la primera ability
             cy.log(response.body.abilities[0].ability);
             //imprime la segunda ability
